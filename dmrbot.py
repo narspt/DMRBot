2a84b7278c65376d180f11e7bd3099bcad82855f
narspt <nars@gmx.net>
2023-09-25 21:08:33 +0100

    multiple improvements on dmrbot.py - the bot now knows who he is talking to, name and callsign (incl. improve speech recognition of callsign) - speech recognition language auto-lock (5 min) after same user talks with the bot 3 times using the same language - answer a default message if user TX with no audio - local language accent for text-to-speech, based on dmr id mcc

--- dmrbot.py
+++ dmrbot.py
@@ -20,16 +20,34 @@ import os
 import sys
 import requests
 import json
+import csv
 import time
 from gtts import gTTS
 
-#requests.packages.urllib3.disable_warnings(requests.packages.urllib3.exceptions.InsecureRequestWarning)
-
 def main():
-    openaiurl = "https://api.openai.com/v1"
     
-    #if len(sys.argv) > 1:
-    #    print(sys.argv[1])
+    try:
+        srcid = int(sys.argv[1])
+    except:
+        srcid = 0
+    
+    callsign = ""
+    name = ""
+    if (srcid > 0) and os.path.isfile("DMRIds.dat"):
+        with open("DMRIds.dat", "r", encoding="utf8", newline="") as ids_file:
+            ids_reader = csv.reader(ids_file, delimiter="\t")
+            for tmp_id in ids_reader:
+                if tmp_id[0] == str(srcid):
+                    print(tmp_id)
+                    callsign = tmp_id[1]
+                    name = tmp_id[2]
+                    break
+    
+    if (not os.path.isfile("DMRIds.dat")) or (time.time() - os.stat("DMRIds.dat").st_mtime > 24*60*60):
+        os.system("{ curl --fail -o DMRIds.tmp -s http://www.pistar.uk/downloads/DMRIds.dat && mv DMRIds.tmp DMRIds.dat; } &")
+    
+    
+    openaiurl = "https://api.openai.com/v1"
     
     try:
         with open("openai_api_key.txt", "r") as file:
@@ -38,7 +56,7 @@ def main():
         print("failed to read file openai_api_key.txt")
         exit(1)
     
-    headers = { "Authorization" : "Bearer " + openai_api_key }
+    headers = { "Authorization": "Bearer " + openai_api_key }
 
 
     ########## Speech-to-Text ##########
@@ -46,39 +64,76 @@ def main():
     print("* Query Whisper API to get Speech-to-Text")
     url = openaiurl + "/audio/transcriptions"
     audio_file_path = "rx.wav"
-
+    
+    # trim silence at start and end of file
+    if os.system("ffmpeg -y -loglevel error -i " + audio_file_path + " -af 'areverse,silenceremove=start_periods=1:start_duration=0.05:start_silence=0.1:start_threshold=0.02,areverse,silenceremove=start_periods=1:start_duration=0.05:start_silence=0.1:start_threshold=0.02' rx-trim.wav") == 0:
+        audio_file_path = "rx-trim.wav"
+    else:
+        print("failed to call ffmpeg")
+    
     data = {
         "model": "whisper-1",
-        "file": audio_file_path,
         "response_format": "verbose_json"
     }
     files = {
         "file": open(audio_file_path, "rb")
     }
-
+    
+    data["prompt"] = "callsign:" + callsign
+    if str(srcid)[0:3] in {"268", "724"}:
+        data["prompt"] += ";language:portuguese"
+    data["prompt"] = "[" + data["prompt"] + "]"
+    #print("prompt=" + data["prompt"])
+    
+    
+    # language auto-lock after user talks with the bot 3 times using the same language
+    last_languages = []
+    if os.path.isfile("last_languages.json"):
+        if time.time() - os.stat("last_languages.json").st_mtime < 300:
+            with open("last_languages.json", "r") as read_file:
+                last_languages = json.load(read_file)[-3:]
+    if len(last_languages) >= 3:
+        for i in range(0, len(last_languages)):
+            if (last_languages[i]["srcid"] != srcid) or (last_languages[i]["lang"] != last_languages[-1]["lang"]):
+                break
+        else:
+            data["language"] = last_languages[-1]["lang"]
+    
     # uncomment the following line to force Speech-to-Text on specific language
     #data["language"] = "pt"
     
-    for i in range(2):
-        if i > 0:
-            time.sleep(2)
-            print("retrying...")
-        try:
-            response = requests.post(url, files=files, data=data, headers=headers, timeout=60)
-            print("Status Code:", response.status_code)
-            if response.status_code == 200:
-                break
-        except requests.exceptions.Timeout:
-            print("connection timeout")
-        except requests.exceptions.RequestException:
-            print("connection error")
-    else:
-        exit(1)
+    if "language" in data:
+        print("Forced Language: " + data["language"])
     
-    speech_to_text = response.json()["text"]
-    speech_language = response.json()["language"]
-    print("Response from Whisper API:", speech_to_text)
-    print("Language:", speech_language)
+
+    if os.stat(audio_file_path).st_size > 44+16000*0.5:
+        for i in range(3):
+            if i > 0:
+                time.sleep(2)
+                print("retrying...")
+            try:
+                response = requests.post(url, files=files, data=data, headers=headers, timeout=30)
+                print("Status Code:", response.status_code)
+                if response.status_code == 200:
+                    break
+            except requests.exceptions.Timeout:
+                print("connection timeout")
+            except requests.exceptions.RequestException:
+                print("connection error")
+        else:
+            exit(1)
+        
+        speech_to_text = response.json()["text"]
+        speech_language = str(response.json()["language"] or "english")
+        print("Response from Whisper API:", speech_to_text)
+        print("Language:", speech_language)
+    else:
+        print("no rx audio")
+        speech_to_text = "answer this text with some variations (talking to the user in a formal way): Hello " + name + ", I am an artificial intelligence assistant, I am here to assist you by providing information and answering your questions. Please speak your questions in clear speech, slowly and formalize them as full questions, just as you would when speaking with another person, avoid to transmit very short sentences like just one or two words because I may have trouble to understand them. How can I help you?"
+        if str(srcid)[0:3] in {"268", "724"}:
+            speech_language = "portuguese"
+        else:
+            speech_language = "english"
 
 
     ########## Query ChatGPT ##########
@@ -97,7 +152,14 @@ def main():
             with open("conversation.json", "r") as read_file:
                 data["messages"] = json.load(read_file)[-8:]
     
-    data["messages"].insert(0,{"role": "system", "content": "You are a voice capable bot, users talk with you using radios, input is processed by speech recognition and output by voice synthetizer. Users may say their callsign on start or end of their communications, ignore that. Current UTC date/time: " + time.strftime("%Y-%m-%d %H:%M",time.gmtime()) })
+    if (len(callsign) > 0) and (len(name) > 0):
+        youAreTalkingWith = "You (assistant) know that you are talking with a ham radio operator, his name is " + name + ", his callsign is " + callsign + ". "
+    elif len(callsign) > 0:
+        youAreTalkingWith = "You (assistant) know that you are talking with a ham radio operator, his callsign is " + callsign + ". "
+    else:
+        youAreTalkingWith = ""
+
+    data["messages"].insert(0,{"role": "system", "content": "You are a voice capable bot, users talk with you using radios, input is processed by speech recognition and output by voice synthetizer. Users may say their callsign on start or end of their communications, ignore that. " + youAreTalkingWith + "Current UTC date/time: " + time.strftime("%Y-%m-%d %H:%M",time.gmtime()) })
     
     # ask for reply in the same language as input
     speech_to_text += " (reply in " + speech_language + ")"
@@ -198,14 +260,26 @@ def main():
     else:
         tts_lang = "en"
     
-    lang_tld = {
-        "en": "us",
-        "fr": "fr",
-        "pt": "pt",
-        "es": "es"
-    }
-    if tts_lang in lang_tld:
-        tts_tld = lang_tld[tts_lang]
+    if os.stat(audio_file_path).st_size > 44+16000*2:
+        last_languages.append({"srcid": srcid, "lang": tts_lang})
+        with open("last_languages.json", "w") as write_file:
+            json.dump(last_languages, write_file, indent = 2)
+    
+    # local language accent
+    if tts_lang == "en":
+        if str(srcid)[0:3] in {"234", "235"}:
+            tts_tld = "co.uk"
+        else:
+            tts_tld = "us"
+    elif tts_lang == "pt":
+        if str(srcid)[0:3] == "724":
+            tts_tld = "com.br"
+        else:
+            tts_tld = "pt"
+    elif tts_lang == "es":
+        tts_tld = "es"
+    elif tts_lang == "fr":
+        tts_tld = "fr"
     else:
         tts_tld = "com"
     
